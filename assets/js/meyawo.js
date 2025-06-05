46052ac5b5deea6ac3c011504035487cbd68b41c
Prottoy Amin <md.jahid.bin.amin@gmail.com>
2025-06-06 02:41:58 +0600

    Initial commit

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,16 +1,4 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
 $(document).ready(function(){
@@ -31,7 +19,6 @@ $(document).ready(function(){
     });
 });
 
-// navbar toggle
 $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
